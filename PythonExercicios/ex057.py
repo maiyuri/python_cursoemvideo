8384387c926918a1e0f233c528bdba4f0b34c017
maiyuri <maiyuri@MacBook-Air-de-Maiyuri.local>
2020-04-22 19:26:04 -0300

    ex057

---
+++ PythonExercicios/ex057.py
@@ -0,0 +1,4 @@
+sexo = input("Você é do sexo F ou M? [F/M] ").upper()
+while sexo != "F" and sexo != "M":
+    print("Digite sexo válido!")
+    sexo = input("Você é do sexo F ou M? [F/M] ").upper()
