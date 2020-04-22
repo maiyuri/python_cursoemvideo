e0e6d394e40edbda0bbcf7ba8f7816330ee93fa8
maiyuri <>
2020-04-22 19:52:18 -0300

    subindo coisa nova

--- PythonExercicios/ex057.py
+++ PythonExercicios/ex057.py
@@ -1,5 +1,6 @@
 sexo = input("Você é do sexo F ou M? [F/M] ").upper()
-
 while sexo != "F" and sexo != "M":
     print("Digite sexo válido!")
     sexo = input("Você é do sexo F ou M? [F/M] ").upper()
+
+print("deu certo")
